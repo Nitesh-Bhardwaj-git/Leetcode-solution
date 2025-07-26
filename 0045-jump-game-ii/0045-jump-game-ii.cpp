671504b0f4fe29cf26ea6b90fcd725e367bce84c
Nitesh Bhardwaj <114833656+Nitesh-Bhardwaj-git@users.noreply.github.com>
2025-07-27 00:34:53 +0530

    Time: 0 ms (100%), Space: 20.4 MB (62.69%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 1; i < nums.size()-1; i++){
+            nums[i] = max(nums[i] + i, nums[i-1]);
+        }
+        int a = 0;
+        while(a < nums.size()-1){
+            ans++;
+            a = nums[a];
+        }
+        return ans;
+    }
+};
